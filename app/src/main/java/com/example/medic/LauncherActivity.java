82589cd3c08428d48fa916f0cc813f36949e1d42
Animila <khristoforov-i@mail.ru>
2023-02-27 13:20:23 +0900

    переход

--- app/src/main/java/com/example/medic/LauncherActivity.java
+++ app/src/main/java/com/example/medic/LauncherActivity.java
@@ -2,13 +2,24 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class LauncherActivity extends AppCompatActivity {
+ public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent onBoard = new Intent(LauncherActivity.this, OnBoardActivity.class);
+                startActivity(onBoard);
+                finish();
+            }
+        }, 3000);
     }
 }
